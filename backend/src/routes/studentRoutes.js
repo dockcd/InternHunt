1e38bc5c887e51aa9352cb2dc60cb8c7c4e442bb
Imad Ibrahim <imadibrahim711@gmail.com>
2025-09-15 09:56:06 +0530

    Delete backend directory

--- backend/src/routes/studentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createStudentDetails } = require('../controllers/studentController');
-
-// POST: Create student academic details
-router.post('/', createStudentDetails);
-
-module.exports = router;
